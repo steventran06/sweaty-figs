3a7e132191e1d2223c8fb39272bf4b0edb2928ac
Steven Tran <steventran06@gmail.com>
2016-05-13 17:50:56 -0700

    (feat) add email intinararies to friends

--- src/containers/ConfirmContainer.js
+++ src/containers/ConfirmContainer.js
@@ -9,10 +9,11 @@ import { addToBuilder,
         createPlan } from '../actions';
 import { bindActionCreators } from 'redux';
 import ConfirmItem from '../components/ConfirmItem';
-import Dialog from 'material-ui/Dialog';
 import TextField from 'material-ui/TextField';
 import Card from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
+import Dialog from 'material-ui/Dialog';
+import {Tabs, Tab} from 'material-ui/Tabs';
 
 var shortid = require('shortid');
 
@@ -21,7 +22,9 @@ export class ConfirmContainer extends Component {
     super(props);
     this.state = {
       planTitle: '',
-      modalOpen: false
+      modalOpen: false,
+      email: null,
+      emails: []
     };
   }
 
@@ -35,13 +38,14 @@ export class ConfirmContainer extends Component {
     this.setState({planTitle: event.target.value});
   }
 
-  // handleSubmit() {
-  //   var plan = {
-      
-  //   }
-  // }
+  handleEmail(event) {
+    this.setState({
+      email: event.target.value
+    });
+  }
 
   saveItinerary() {
+    this.toggleModal();
     this.props.createPlan(Object.assign({}, {
       user_id: auth.token.user_id,
       clientside_id: shortid.generate(),
@@ -49,7 +53,34 @@ export class ConfirmContainer extends Component {
       desc: '',
       likes: 0
     }), activityIds, (response) => console.log('saved to db, response: ', response));
-    this.toggleModal();
+  }
+
+  selectConfirmTab() {
+    this.setState({
+      value: 'confirm'
+    });
+  }
+
+  SelectShareTab() {
+    this.setState({
+      value: 'share'
+    });
+  }
+
+  addEmail() {
+    var emailsArr = this.state.emails.slice();
+    emailsArr.push(this.state.email.trim());
+    this.setState({
+      emails: emailsArr
+    });
+  }
+
+  removeEmail(email){
+    console.log(email);
+  }
+
+  emailPlan() {
+    window.open(`mailto:${this.state.emails.join(',')}?Subject=${this.state.planTitle}`);
   }
 
   render() {
@@ -66,7 +97,15 @@ export class ConfirmContainer extends Component {
     for (var i = 0; i < planBuilder.length; i++) {
       activityIds.push({id: planBuilder[i].id});
     }
-    
+
+    const actions = [
+          <FlatButton
+            label="Thanks"
+            primary={true}
+            onClick={this.toggleModal.bind(this)}
+          />
+        ];
+
     return (
       <Card>
       <TextField
@@ -84,7 +123,6 @@ export class ConfirmContainer extends Component {
             onDeleteFromBuilderClicked={() => this.props.deleteFromBuilder(activity)}
             onMoveUpClicked={() => {
               this.props.reorderUp(planBuilder.indexOf(activity));
-              
             }}
             onMoveDownClicked={() => {
               this.props.reorderDown(planBuilder.indexOf(activity));
@@ -96,25 +134,64 @@ export class ConfirmContainer extends Component {
           Save Itinerary
         </FlatButton>
         <Dialog
+          title="You have saved your itinerary!"
+          actions={actions}
+          modal={true}
           open={this.state.modalOpen}>
-          Hello
+          <Tabs
+            value={this.state.value}>
+            <Tab
+              value="confirm"
+              label="CONFIRM"
+              onClick={this.selectConfirmTab.bind(this)}>
+                Tab One
+            </Tab>
+            <Tab
+              value="share"
+              label="SHARE"
+              onClick={this.SelectShareTab.bind(this)}>
+              <div className="container">
+              <div className="row">
+                <h3>Share with your Facebook friends</h3>
+              </div>
+                <div className="row">
+                  <h3>Email your itinerary to your friends!</h3>
+                  <div className="col-sm-4">
+                    <TextField
+                      className="text-field"
+                      id="send-field"
+                      type="send"
+                      onChange={this.handleEmail.bind(this)}
+                      placeholder="Email"
+                      style={{marginBottom: 15}} /><br />
+                    <br />
+                    <FlatButton
+                      label="Add"
+                      primary={true}
+                      onClick={this.addEmail.bind(this)}
+                    />
+                    <FlatButton
+                      label="Email Itinerary"
+                      primary={true}
+                      onClick={this.emailPlan.bind(this)}
+                    />
+                  </div>
+                  <div className="col-sm-4" style={{marginTop: 15}}>
+                    {this.state.emails.length > 0 ? <b>Emails Added<br /></b> : null}
+                    {this.state.emails.length > 0 ? this.state.emails.map((email) => {
+                      return <em onClick={this.removeEmail.bind(this, email)}>{email}<br/></em>
+                    }) : null}
+                  </div>
+                </div>
+              </div>
+            </Tab>
+          </Tabs>
         </Dialog>
       </Card>
     )
   }
 
 }
-
-// ConfirmContainer.propTypes = {
-//   activities: PropTypes.arrayOf(PropTypes.shape({
-//     id: PropTypes.number,
-//     title: PropTypes.string.isRequired,
-//     desc: PropTypes.string.isRequired,
-//     // categories: PropTypes.array.isRequired,
-//     city: PropTypes.string.isRequired
-//   })).isRequired,
-//   saveToDb: PropTypes.func.isRequired
-// }
 
 const mapStateToProps = (state) => {
   return {
